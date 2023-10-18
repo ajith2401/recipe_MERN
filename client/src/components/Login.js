363bac6da2cbbb312d0e3fe9407e24fc25d703fb
ajith <ajith24ram@gmail.com>
2023-10-18 16:22:01 +0530

    completing basic signup flow

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import {useForm} from 'react-hook-form'
+
 
 function Login() {
     const navigateTo = useNavigate();
-    const form  = useForm();
-    const { register, control} = form
+   
+
   return ( 
     <div>
     <form id='loginForm'>
     <h2> Login </h2>
-    <input type='text' placeholder='email or phone nuber' name='emailOrPhonenumber' id='emailOrPhonenumber' {...register("emailOrPhonenumber", {required : "please enter the mail u=id or phone num ber"})}/>
-    <input type='password' name='password' id='password' placeholder='password' {...register("password")}/>
+    <input type='text' placeholder='email or phone nuber' name='emailOrPhonenumber' id='emailOrPhonenumber'/>
+    <input type='password' name='password' id='password' placeholder='password'/>
     <button id='loginBtn' onClick={()=>navigateTo('/home')}>Login</button>
     </form>
-    <p onClick={()=>navigateTo('/forgotPassword')}>forgot password</p> 
+    <p onClick={() => navigateTo('/forgotPassword')}>Forgot Password</p>
     <hr></hr>
     <button id='createAccBtn' onClick={()=>navigateTo('./signup')}>create an account</button>
     </div>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,21 +1,110 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 
 function Signup() {
-    const navigateTo = useNavigate()
-  return (
+  const navigateTo = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [passwordErr, setPasswordErr] = useState(false);
+
+  const hideError = () => {
+    setError(false);
+  };
+
+  const hanldeChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const hanldePassword = (e) => {
+    if (e.target.value !== formData.password) {
+      setPasswordErr("Password does not match");
+    } else {
+      setPasswordErr(false);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("http://localhost:8080/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        // Automatically hide the error message after 0.5 seconds
+        setTimeout(() => {
+          hideError();
+        }, 500);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigateTo('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+      // Automatically hide the error message after 0.5 seconds
+      setTimeout(() => {
+        hideError();
+      }, 500);
+    }
+  };
+
+  return ( 
     <div>
-      <form id='signUpForm'>
       <h2> Signup </h2>
-      <input type='text' name='firstName' placeholder='first name' required/>
-      <input type='text' name='lastName' placeholder='last name'/>
-      <input type='password' id='password' placeholder='password' required/>
-      <input type='password' id='Re-password' placeholder='Re enter password' required/>
-      <input type='text'  placeholder='email or phonenumber' required/>
-      <button onClick={()=>navigateTo('/home')}>Signup</button>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <form id='signUpForm' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='firstName'
+          placeholder='first name'
+          required
+          onChange={hanldeChange}
+        />
+        <input type='text' name='lastName' placeholder='last name' onChange={hanldeChange} />
+        <input
+          type='password'
+          name='password'
+          id='password'
+          placeholder='password'
+          required
+          onChange={hanldeChange}
+        />
+        {passwordErr && <small>{passwordErr}</small>}
+        <input
+          type='password'
+          name='Re-password'
+          id='Re-password'
+          placeholder='Re enter password'
+          required
+          onChange={hanldePassword}
+        />
+        <input
+          type='text'
+          name='emailOrPhnum'
+          placeholder='email or phonenumber'
+          required
+          onChange={hanldeChange}
+        />
+        <button id='signupBtn' disabled={loading}>
+          {loading ? "loading" : "Signup"}
+        </button>
       </form>
+      <p>have an account </p> <Link to='/'>sign in </Link>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
