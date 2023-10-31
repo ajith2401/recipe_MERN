f83dd7784f028fe07deb53972f4d9f5e90eb9db0
ajith <ajith24ram@gmail.com>
2023-10-31 17:50:40 +0530

    jwt vslue hard coded

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,13 +2,6 @@ import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js"
 import jwt from 'jsonwebtoken'
-import dotenv from "dotenv";
-
-// Load environment variables from a .env file (this is optional in a production environment)
-dotenv.config();
-
-// Now, you can access your environment variable
-const jwtSecret = process.env.JWT_SECRET;
 
 export const signup = async (req,res,next) =>{
    const {firstName,lastName,password,emailOrPhoneNumber} = req.body
@@ -34,7 +27,7 @@ export const signin = async (req,res,next)=>{
           if (!validPassword) {
             return next(errorHandler(401, "invalid credentials"));
           }          
-        const token = jwt.sign({id : validUser._id}, jwtSecret)
+        const token = jwt.sign({id : validUser._id}, "1d50c142-cec3-45b5-b741-8701b4f233b0")
         const { password: pass, ...restVal } = validUser._doc;
         res.cookie("access_token", token, { httpOnly: true, domain: "https://ajith-recipe-app.onrender.com" }).json(restVal)
     } catch (error) {
@@ -48,7 +41,7 @@ export const google = async (req, res, next) => {
     const existingUser = await User.findOne({ emailOrPhoneNumber: req.body.emailOrPhoneNumber });
     if (existingUser) {
       // A user with the same email/phone number already exists, handle this case
-      const token = jwt.sign({ id: existingUser._id }, jwtSecret);
+      const token = jwt.sign({ id: existingUser._id }, "1d50c142-cec3-45b5-b741-8701b4f233b0");
       const { password, ...restVal } = existingUser._doc;
       res.cookie("access_token", token, { httpOnly: true }).status(200).json(restVal);
     } else {
@@ -66,7 +59,7 @@ export const google = async (req, res, next) => {
         linkedIn: "",
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, jwtSecret);
+      const token = jwt.sign({ id: newUser._id }, "1d50c142-cec3-45b5-b741-8701b4f233b0");
       const { password, ...restVal } = newUser._doc;
       res.cookie("access_token", token, { httpOnly: true, domain: "https://ajith-recipe-app.onrender.com" }).json(restVal)
     }
--- server/utils/verifyuser.js
+++ server/utils/verifyuser.js
@@ -1,8 +1,5 @@
 import { errorHandler } from "./error.js";
 import jwt from 'jsonwebtoken';
-import dotenv from "dotenv";
-
-const jwtSecret = process.env.JWT_SECRET;
 
 export const verifyToken = (req, res, next) => {
     const accessToken = req.cookies.access_token;
@@ -11,7 +8,7 @@ export const verifyToken = (req, res, next) => {
         return next(errorHandler(401, "Unauthorized: Access token missing."));
     }
 
-    jwt.verify(accessToken, jwtSecret, (error, user) => {
+    jwt.verify(accessToken, "1d50c142-cec3-45b5-b741-8701b4f233b0", (error, user) => {
         if (error) {
             return next(errorHandler(403, "Forbidden: Invalid access token."));
         }
