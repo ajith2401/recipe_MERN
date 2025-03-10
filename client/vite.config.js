9e76e6d7a722f467969648d3811d60bd1c7b5b69
ajith <ajith24ram@gmail.com>
2025-03-11 00:26:09 +0530

    test push

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ['@mui/x-date-pickers'],
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
