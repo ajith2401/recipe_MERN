a8220b885f1d6822cad7efdbde219522cfd1b5e0
ajith <ajith24ram@gmail.com>
2025-03-11 00:47:55 +0530

    iii

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,28 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@mui/x-date-pickers'],
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true // Add for better debugging
-  },
-  resolve: {
-    alias: {
-      stream: 'stream-browserify',
-      crypto: 'crypto-browserify'
-    }
-  },
-  server: {
-    port: 5173,
-    strictPort: true
-  },
-  // Add this if you're using environment variables
-  define: {
-    'process.env': {}
-  }
 })
