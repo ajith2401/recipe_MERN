8480135f0c0dc3926099d8246e05568f56953d0b
ajith <ajith24ram@gmail.com>
2025-03-11 19:14:09 +0530

    small fix 08

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ import http from 'http'
 import notifyRouter from "./routes/notify.router.js";
 import chatRouter from "./routes/chat.router.js";
 
+
 dotenv.config()
 const app = express();
 const server = http.createServer(app);
